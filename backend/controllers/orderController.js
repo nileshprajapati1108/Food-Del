5b29600cce768d777cab059594b48325005ddea7
Nilesh Prajapati <nileshprajapati1108@gmail.com>
2026-01-03 11:50:54 +0530

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -6,7 +6,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 // PLACE ORDER
 const placeOrder = async (req, res) => {
-  const frontend_url = "https://food-del-frontend-rvuy.onrender.com/myorder";
+  const frontend_url = "https://food-del-frontend-rvuy.onrender.com/myorders";
 
   try {
     const newOrder = new orderModel({
