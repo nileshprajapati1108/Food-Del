8f8d9f7785e1b2bd8d6de7fe141f628ba9831467
Nilesh <nileshprajapati1108@gmail.com>
2026-01-03 11:01:37 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -11,17 +11,11 @@ import orderRouter from "./routes/orderRoute.js";
 
 //app config
 const app =express()
-const port = 4000
+const port = process.env.PORT || 4000;
 
 //middleware
 app.use(express.json())
-app.use(cors({
-  origin: "https://food-del-nine-swart.vercel.app",
-  methods: ["GET","POST","PUT","DELETE"],
-  credentials: true
-}))
-
-
+app.use(cors())
 
 //db connection
 connectDb();
