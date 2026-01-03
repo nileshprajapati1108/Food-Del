928d64d2e090484cc014b12fceee557bd07a3008
Nilesh Prajapati <nileshprajapati1108@gmail.com>
2026-01-03 13:04:04 +0530

    Update Verify.jsx

--- frontend/src/pages/Verify/Verify.jsx
+++ frontend/src/pages/Verify/Verify.jsx
@@ -1,49 +1,38 @@
-import React, { useContext, useEffect } from "react";
-import "./Verify.css";
-import { useNavigate, useSearchParams } from "react-router-dom";
-import { StoreContext } from "../../context/StoreContext";
-import axios from "axios";
-
+import React from 'react'
+import "./Verify.css"
+import { useNavigate, useParams, useSearchParams } from 'react-router-dom'
+import { useContext } from 'react';
+import { StoreContext } from '../../context/StoreContext';
+import axios from 'axios';
+import { useEffect } from 'react';
 const Verify = () => {
-  const [searchParams] = useSearchParams();
-  const success = searchParams.get("success");
-  const orderId = searchParams.get("orderId");
 
-  const { url } = useContext(StoreContext);
-  const navigate = useNavigate();
+  const [searchParams,setSearchParams] = useSearchParams();
+  const success = searchParams.get("success")
+  const orderId = searchParams.get("orderId")
 
-  useEffect(() => {
-    const verifyPayment = async () => {
-      try {
-        const response = await axios.post(
-          `${url}/api/order/verify`,
-          { success, orderId }
-        );
+  const {url} = useContext(StoreContext);
+  const navigate =useNavigate();
 
-        if (response.data.success) {
-          navigate("/myorders");
-        } else {
-          navigate("/");
-        }
-      } catch (error) {
-        console.error("Payment verification failed", error);
-        navigate("/");
-      }
-    };
-
-    if (orderId && success) {
-      verifyPayment();
-    } else {
-      navigate("/");
-    }
-  }, []);
-
+const verifyPayment = async()=>{
+  const response = await axios.post(url+"/api/order/verify",{success,orderId});
+  if(response.data.success){
+navigate("/myorders");
+  }
+  else{
+    navigate("/")
+  }
+}
+useEffect(()=>{
+verifyPayment();
+},[])
   return (
-    <div className="verify">
-      <div className="spinner"></div>
-      <p>Verifying your payment...</p>
-    </div>
-  );
-};
+    <div className='verify'>
+      <div className="spinner">
 
-export default Verify;
+      </div>
+    </div>
+  )
+}
+
+export default Verify 
